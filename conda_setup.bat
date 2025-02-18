0aa9b24616ea1cecd0bc9a319c2257ce1979220a
Daniel Ford <77839711+danielford166@users.noreply.github.com>
2025-02-18 11:51:15 +0000

    Updating conda_setup.bat

--- conda_setup.bat
+++ conda_setup.bat
@@ -1,1 +1,1 @@
-conda env create --file environment_JamieLab_Teaching.yml && conda activate JamieLab_Teaching
+conda env remove --name JamieLab_Teaching && conda env create --file environment_JamieLab_Teaching.yml && conda activate JamieLab_Teaching
